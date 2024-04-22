954f37e7fede5eb14e295cb396858b23a8154d94
KmInno <innocentmuwanda0@gmail.com>
2024-04-22 17:51:55 +0300

    Week01
    week01 assignments

---
+++ scripts/getdataes.js
@@ -0,0 +1,7 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("year").textContent = currentYear;
+
+const lastModifiedDate = document.lastModified;
+
+document.getElementById("last-modified").textContent = lastModifiedDate;
