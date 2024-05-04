e1480cfc32fe0f9e1a46a9e517f45699fe74041e
KmInno <innocentmuwanda0@gmail.com>
2024-05-05 00:32:43 +0300

    new

--- scripts/temples.js
+++ scripts/temples.js
@@ -8,12 +8,12 @@ window.onload = function () {
   displayDateModified();
 };
 
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
+const mainnav = document.querySelector("nav ul");
+const hambutton = document.querySelector("#menu");
 
-hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle('show');
-	hambutton.classList.toggle('show');
+hambutton.addEventListener("click", function() {
+  mainnav.classList.add('show-menu');
+  hambutton.classList.toggle("show");
 });
 
 
